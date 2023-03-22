b5f8d080e3caa330a0a2ac6b443314ee17fb917a
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2023-03-22 21:46:11 +0600

    email registration for single event

--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,18 +1,58 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 const SingleEvent = ({ event }) => {
+    const inputEmail = useRef();
+    const router = useRouter();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        const emailValue = inputEmail.current.value;
+        const eventId = router?.query.id;
+
+        try {
+            const response = await fetch('/api/email-registration', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: emailValue,
+                    eventId,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+            const data = await response.json();
+        } catch (error) {
+            console.log('ERROR', error);
+        }
+    };
+
     return (
-        <div>
+        <div className="event_single_page">
+            <h1>{event.title}</h1>
             <Image
                 src={event.image}
                 alt={event.title}
                 width={1000}
                 height={500}
             />
-            <h1>{event.title}</h1>
             <p>{event.description}</p>
-            <input type="email" name="" id="" />
-            <button>Submit</button>
+            <form onSubmit={onSubmit} className="email_registration">
+                <label htmlFor="register">Get registered for this event!</label>
+                <input
+                    ref={inputEmail}
+                    type="email"
+                    name="register"
+                    id="email"
+                    placeholder="Please insert your email here"
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
